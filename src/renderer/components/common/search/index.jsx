241a009b38ca2d9a7e1b001f5ce64d7c7241f57d
renzhenguo_v <renzhenguo_v@didiglobal.com>
2019-11-06 15:38:22 +0800

    丰富字体图标与search搜索框

--- src/renderer/components/common/search/index.jsx
+++ src/renderer/components/common/search/index.jsx
@@ -1,16 +1,42 @@
 import React, { useState } from 'react';
+import CSSModules from 'react-css-modules';
 import style from './index.less';
 
 function Search(props) {
+  const [showHistory, setShowHistory] = useState(false);
+
+  const onKeyDown = (e) => {
+    if(e.keyCode == 13){
+      // console.log(e.target.value)
+    }
+  }
+  const onChange = (e) => {
+    // console.log(e.target.value)
+  }
+  const onFocus = () => {
+    setShowHistory(true)
+    console.log(showHistory)
+  }
+  const onBlur = () => {
+    setShowHistory(false)
+    console.log(showHistory)
+  }
+
+  
   return (
-    <div className={ style.search }>
+    <div styleName='search'>
       <input
-        className={ style.input }
+        styleName='input'
         placeholder="搜索"
+        onKeyDown={ onKeyDown }
+        onChange={ onChange }
+        onFocus={ onFocus }
+        onBlur={ onBlur }
       />
-      <span className="iconfont">&#xe800;</span>
+      <span styleName="searchIcon" className='iconfont'>&#xe741;</span>
+      {/*<span styleName="clearIcon" className='iconfont'>&#xe6e9;</span>*/}
     </div>
   );
 }
 
-export default Search;
+export default CSSModules(Search,style);
