1ab5f60656e5741a650527aba989260e406e337b
YangGeGe <2295185835@qq.com>
2019-11-06 19:21:22 +0800

    fix： eslint fix

--- src/renderer/components/common/search/index.jsx
+++ src/renderer/components/common/search/index.jsx
@@ -6,37 +6,37 @@ function Search(props) {
   const [showHistory, setShowHistory] = useState(false);
 
   const onKeyDown = (e) => {
-    if(e.keyCode == 13){
+    if (e.keyCode === 13) {
       // console.log(e.target.value)
     }
-  }
+  };
   const onChange = (e) => {
     // console.log(e.target.value)
-  }
+  };
   const onFocus = () => {
-    setShowHistory(true)
-    console.log(showHistory)
-  }
+    setShowHistory(true);
+    console.log(showHistory);
+  };
   const onBlur = () => {
-    setShowHistory(false)
-    console.log(showHistory)
-  }
+    setShowHistory(false);
+    console.log(showHistory);
+  };
 
-  
+
   return (
-    <div styleName='search'>
+    <div styleName="search">
       <input
-        styleName='input'
+        styleName="input"
         placeholder="搜索"
         onKeyDown={ onKeyDown }
         onChange={ onChange }
         onFocus={ onFocus }
         onBlur={ onBlur }
       />
-      <span styleName="searchIcon" className='iconfont'>&#xe741;</span>
-      {/*<span styleName="clearIcon" className='iconfont'>&#xe6e9;</span>*/}
+      <span styleName="searchIcon" className="iconfont">&#xe741;</span>
+      {/* <span styleName="clearIcon" className='iconfont'>&#xe6e9;</span> */}
     </div>
   );
 }
 
-export default CSSModules(Search,style);
+export default CSSModules(Search, style);
