4eb51bd99f9675956e22df112f621bf10f9670b4
YangGeGe <2295185835@qq.com>
2019-11-09 20:12:18 +0800

    feat: add 功能map

--- src/renderer/components/constraint.js
+++ src/renderer/components/constraint.js
@@ -1,1 +1,30 @@
 // 一些映射
+export const MODULAR_ITEM_MAP = {
+  FIND_MUSIC: '发现音乐',
+  FRINDS: '朋友',
+  LOCAL_MUSIC: '本地音乐',
+  DOWLOAD_MANAGE: '下载管理',
+  MY_COLLECTION: '我的收藏',
+};
+
+const USER_MODULAR_ITEM = [
+  'LOCAL_MUSIC',
+  'DOWLOAD_MANAGE',
+  'MY_COLLECTION',
+];
+
+const RECOMMEND_MODULAR_ITEM = [
+  'FIND_MUSIC',
+  'FRINDS',
+];
+
+export const Modular = [
+  {
+    title: '推荐',
+    modular: USER_MODULAR_ITEM,
+  },
+  {
+    title: '我的音乐',
+    modular: RECOMMEND_MODULAR_ITEM,
+  },
+];
