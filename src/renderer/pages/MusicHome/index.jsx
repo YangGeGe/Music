80d5a0ce52a0b4ea5ecc789cb8bd79845a9c7cd2
renzhenguo_v <renzhenguo_v@didiglobal.com>
2019-11-15 15:02:39 +0800

    背景颜色修改

--- src/renderer/pages/MusicHome/index.jsx
+++ src/renderer/pages/MusicHome/index.jsx
@@ -33,17 +33,56 @@ function MusicHome() {
   };
   return (
     <div styleName='context'>
-      <Banner/>
-      <div styleName='searchAndList'>
-        <Search
-          onKeyDown={onKeyDown}
-          onChange={onChange}
-          onFocus={onFocus}
-          onBlur={onBlur}
-          showHistory={showHistory}
-          inputContent={inputContent}
-        />
-        <HomePage/>
+      <div styleName='contextTop'>
+        <Banner styleName='banner'/>
+        <div styleName='searchAndList'>
+          <Search
+            onKeyDown={onKeyDown}
+            onChange={onChange}
+            onFocus={onFocus}
+            onBlur={onBlur}
+            showHistory={showHistory}
+            inputContent={inputContent}
+          />
+          <div styleName='rankingTitle'>热门排行版</div>
+          <div styleName='contextLists'>
+            <div styleName='contextList'>
+              <img src='http://p2.music.126.net/eBF7bHnJYBUfOFrJ_7SUfw==/109951163351825356.jpg?param=34y34' />
+              <div>
+                <span styleName='musicTitle'>后来</span>
+                <span>刘若英</span>
+              </div>
+            </div>
+            <div styleName='contextList'>
+              <img src='http://p2.music.126.net/eBF7bHnJYBUfOFrJ_7SUfw==/109951163351825356.jpg?param=34y34' />
+              <div>
+                <span styleName='musicTitle'>后来</span>
+                <span>刘若英</span>
+              </div>
+            </div>
+            <div styleName='contextList'>
+              <img src='http://p2.music.126.net/eBF7bHnJYBUfOFrJ_7SUfw==/109951163351825356.jpg?param=34y34' />
+              <div>
+                <span styleName='musicTitle'>后来</span>
+                <span>刘若英</span>
+              </div>
+            </div>
+            <div styleName='contextList'>
+              <img src='http://p2.music.126.net/eBF7bHnJYBUfOFrJ_7SUfw==/109951163351825356.jpg?param=34y34' />
+              <div>
+                <span styleName='musicTitle'>后来</span>
+                <span>刘若英</span>
+              </div>
+            </div>
+            <div styleName='contextList'>
+              <img src='http://p2.music.126.net/eBF7bHnJYBUfOFrJ_7SUfw==/109951163351825356.jpg?param=34y34' />
+              <div>
+                <span styleName='musicTitle'>后来</span>
+                <span>刘若英</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
