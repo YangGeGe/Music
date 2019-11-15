f291555910587a44c57e87f5a98916c8a26765ef
YangGeGe <2295185835@qq.com>
2019-11-15 19:33:28 +0800

    feat： axios

--- src/renderer/request/request.js
+++ src/renderer/request/request.js
@@ -1,2 +1,21 @@
 import axios from 'axios';
-// TODO:请求封装
+
+let baseURL = '';
+if (process.env.NODE_ENV === 'development') {
+  baseURL = 'http://localhost:3000';
+} else {
+  // baseURL = '';
+}
+const request = axios.create({
+  baseURL,
+  timeout: 10000,
+});
+
+request.interceptors.request.use(function (config) {
+  // 在发送请求之前做些什么
+  return config;
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+})
+;
