89b22e9d84039e1500670a670c147f48cb9bd4e2
YangGeGe <36037549+YangGeGe@users.noreply.github.com>
2019-11-25 16:23:40 +0800

    Update index.jsx

--- src/renderer/components/common/Button/index.jsx
+++ src/renderer/components/common/Button/index.jsx
@@ -1,13 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import CSSModules from 'react-css-modules';
 import PropTypes from 'prop-types';
 import style from './index.less';
 
 function Button(props) {
   const { type, children } = props;
-  useEffect(() => {
-    
-  });
   return (
     <button
       styleName={type || 'button'}
@@ -16,8 +13,10 @@ function Button(props) {
     </button>
   );
 }
+
 Button.propTypes = {
   type: PropTypes.string,
   children: PropTypes.string,
 };
+
 export default CSSModules(Button, style);
