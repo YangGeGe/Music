7b7690f19d162bdb5784882b78338020e42b3a53
e <2295185835@qq.com>
2019-12-18 20:23:48 +0800

    fix: drag css

--- src/renderer/components/MusicHome/SiderBar/Dragabled.jsx
+++ src/renderer/components/MusicHome/SiderBar/Dragabled.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Dragabled() {
   return (
-    <div style={ { height: 30, webkitAppRegion: 'drag' } }/>
+    <div style={ { height: 30, WebkitAppRegion: 'drag' } }/>
   );
 }
 
